1dc7369d97331f98b4440816700686f3051b2d32
Rithik Sachdev <39690921+rithiksachdev@users.noreply.github.com>
2023-11-29 12:04:38 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,16 @@
-from card import Card
+from card import Card, copper, silver, gold, estate, duchy, province
 from player import Player
 
 class Game:
     def __init__(self, player_names):
         self.players = [Player(name) for name in player_names]
-        self.supply = [Card("Gold", 6, "Treasure"), Card("Silver", 3, "Treasure"), Card("Estate", 2, "Victory")]
+        self.supply = [copper, silver, gold, estate, duchy, province] * 10  # Simplified supply
         self.initialize_game()
 
     def initialize_game(self):
         # Initialize players' decks and the supply
         for player in self.players:
-            player.deck = [Card("Copper", 0, "Treasure")] * 7 + [Card("Estate", 2, "Victory")] * 3
+            player.deck = [copper] * 7 + [estate] * 3
             # Shuffle each player's deck
 
     def play_game(self):
@@ -18,7 +18,17 @@ class Game:
         while not self.is_game_over():
             current_player = self.players[turn % len(self.players)]
             current_player.play_turn(self.supply)
+            self.player_turn_actions(current_player)
             turn += 1
+
+    def player_turn_actions(self, player):
+        # Example: Player can buy a card
+        print("Available cards to buy: ", ', '.join(str(card) for card in self.supply))
+        buy_choice = input(f"{player.name}, choose a card to buy or type 'skip': ")
+        if buy_choice.lower() != 'skip':
+            chosen_card = next((card for card in self.supply if card.name.lower() == buy_choice.lower()), None)
+            if chosen_card:
+                player.buy_card(chosen_card, self.supply)
 
     def is_game_over(self):
         # Define the game over condition (e.g., a certain number of piles are empty)
