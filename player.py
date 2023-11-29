6a054898209664fd3ed2d5a49080e4256c58cd90
Rithik Sachdev <39690921+rithiksachdev@users.noreply.github.com>
2023-11-29 12:06:27 -0500

    Update player.py

--- player.py
+++ player.py
@@ -17,6 +17,18 @@ class Player:
         self.draw_card()
         print(f"{self.name}'s hand: {', '.join(str(card) for card in self.hand)}")
         # Additional actions would go here
-        # Implement buying cards from the supply here
 
-# Further functions can be added to manage the player's actions, hand, deck, etc.
+    def buy_card(self, card, supply):
+        if card in supply and self.can_afford(card):
+            self.discard_pile.append(card)
+            supply.remove(card)
+            print(f"{self.name} bought {card.name}")
+        else:
+            print("Cannot buy this card")
+
+    def can_afford(self, card):
+        # Simplified: count number of treasure cards in hand
+        treasure = sum(c.card_type == "Treasure" for c in self.hand)
+        return treasure >= card.cost
+
+    # More functions for actions, playing cards, etc., can be added
