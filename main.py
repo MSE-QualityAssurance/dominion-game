296da75dfe44aecbca9af51235605268046757c5
Rithik Sachdev <39690921+rithiksachdev@users.noreply.github.com>
2023-11-29 12:02:45 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from game import Game
+
+def main():
+    player_names = ["Player 1", "Player 2"]  # Extend this list for more players
+    game = Game(player_names)
+    game.play_game()
+
+if __name__ == "__main__":
+    main()
